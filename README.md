a4d640e145601140bcc2b2b7cd49ccacfdb7ce7f
zcm0705 <cuimei@brandeis.edu>
2017-06-14 18:24:10 +0800

    add pagination

--- README.md
+++ README.md
@@ -1,5 +1,75 @@
 # python
+Crawl with Python
+Target Website: https://learn.adafruit.com/
+
+Function:
+	Crawl every single guide-block on the web page. Go to the guide page and grab info(side bar/main content/related guides). Pagination is enabled.
+
 Need to improve:
-  1. Make it more clever. Reduce dependency on class name.
-  2. To be scalable so that it can crawl other web pages with similar design such as "Light Sensor"
-  3. Organize the data with more readable format.
+  1. Finish the main content part. Come up with a general way to crawl the main content info for different guide page.
+  2. Organize the data with more readable format.
+  3. User can scrap any webpage from the "Learn" section when an URL parameter is added to the end of the command (e.g. `./crawler.py -l [URL]`) in terminal
+
+Data sample:
+
+ISS Pin
+{
+    "Guide Info": {
+        "Author": "Leslie Birch", 
+        "Description": "A Particle Photon microcontroller and an Adafruit Neopixel ring combine to make a pin that's fit for NASA fans. It displays an orbiting white blip when idle and then turns blue, white and red when the ISS flies by. The code makes use of IFTTT (If This Then That), a free site that makes connecting IoT devices as easy as a few clicks. This project was inspired by my first NASA Space Apps Challenge with friend Brooks Rampersad--an ISS Orbit Skirt.", 
+        "Link": "https://learn.adafruit.com/iss-pin", 
+        "Tagline": "You'll be star gazing when this NeoPixel pin lights up to notify you of the International Space Station flying overhead.", 
+        "Title": "ISS Pin"
+    }, 
+    "Page Info": {
+        "Left Sidebar": {
+            "Subtitle": "You'll be star gazing when this NeoPixel pin lights up to notify you of the International Space Station flying overhead.", 
+            "Title": "ISS Pin", 
+            "Topics": [
+                "Overview", 
+                "Tools & Supplies", 
+                "Solder Circuit", 
+                "Create Cover", 
+                "Code", 
+                "Set Up IFTTT", 
+                "Wear It!"
+            ]
+        }, 
+        "Related Guides": [
+            "Circuit Playground Class Scheduler", 
+            "LED Masquerade Masks", 
+            "Light-Up Angler Fish Embroidery", 
+            "Flora TSL2561 Lux Sensor"
+        ]
+    }
+}
+NXP Precision 9DoF Breakout
+{
+    "Guide Info": {
+        "Author": "Kevin Townsend", 
+        "Description": "This learning guide gives you everything you need to get started using out high precision 9DoF breakout board based on NXP's FXOS8700 and FXAS21002 sensors.", 
+        "Link": "https://learn.adafruit.com/nxp-precision-9dof-breakout", 
+        "Tagline": "Getting up and running (literally?) with our high precision orientation breakout board.", 
+        "Title": "NXP Precision 9DoF Breakout"
+    }, 
+    "Page Info": {
+        "Left Sidebar": {
+            "Subtitle": "Getting up and running (literally?) with our high precision orientation breakout board.", 
+            "Title": "NXP Precision 9DoF Breakout", 
+            "Topics": [
+                "Overview", 
+                "Pinout", 
+                "Wiring & Test", 
+                "Calibration (USB)", 
+                "Orientation Test (USB)", 
+                "Downloads"
+            ]
+        }, 
+        "Related Guides": [
+            "Adafruit 128x64 OLED Bonnet for Raspberry Pi", 
+            "Sensors in MakeCode", 
+            "Digital Fidget Spinner", 
+            "Hydro Dipping 3D Prints"
+        ]
+    }
+}
--- crawler.py
+++ crawler.py
@@ -1,73 +1,116 @@
+'''
+@Author Jasmine Zhang
+@Date June 14 2017
+'''
+
 from bs4 import BeautifulSoup
 import requests
 import json
 
-def spider():	
-	# prepare soup
-	url = 'https://learn.adafruit.com/sensors-in-makecode/buttons'
+def crawl():
+
+	database = {}
+	# the url for the first page is https://learn.adafruit.com/, need to fix later
+	page_prefix = 'https://learn.adafruit.com/guides?guide_page='
+	page = 1
+	max_pages = 5
+	while page <= max_pages:
+		page_url = page_prefix + str(page)
+		page_soup = cook_soup(page_url)
+		for guide_block in page_soup.findAll('div', class_ = 'guide-block'):
+			# convert from unicode to str type
+			title_name = (guide_block.find(class_ = 'title').a.get_text().strip()).encode('utf-8')
+			database[title_name] = {}
+
+			# crawl brief info
+			database[title_name]['Guide Info'] = crawl_guide_block(guide_block, title_name)
+			
+			# crawl item page
+			item_url = get_item_url(guide_block)
+			print title_name
+			database[title_name]['Page Info'] = crawl_item_page(item_url)
+			convert2json(database[title_name])
+		page += 1
+
+def crawl_guide_block(soup, title):
+	guide = {'Title': title, 
+		'Tagline': soup.find(class_ = 'tagline').a.get_text().strip(), 
+		'Author': soup.find(class_ = "author").a.get_text().strip(), 
+		# 'Img': soup.a.img.get('src'),  fix it later
+		# 'Link': soup.find(class_ = 'title').a.get('href').strip(),
+		'Link': get_item_url(soup).strip(),
+		'Description': soup.find(class_ = "description").get_text().strip()}
+	return guide
+
+def get_item_url(soup):
+	return 'https://learn.adafruit.com' + soup.find(class_ = 'title').a.get('href').strip()
+
+def cook_soup(url):
 	plain_text = requests.get(url).text
 	soup = BeautifulSoup(plain_text, "html.parser")
+	return soup
+
+def convert2json(database):
+	json_object = json.dumps(database, sort_keys=True, indent=4)
+	print json_object
+
+def crawl_item_page(url):
+	# prepare soup
+	soup = cook_soup(url)
 	
 	# use Dict structore to store data
-	dict = {}
+	page_info = {}
 
 	# divide the web page into three parts and crawl them seperately
-	crawl_sidebar(soup.find("div", class_ = "sidebar-left-content"), dict)
-	crawl_article(soup.find("article"), dict)
-	crawl_related_guides(soup.find(class_ = "related-guides-content row"), dict)
+	crawl_sidebar(soup.find("div", class_ = "sidebar-left-content"), page_info)
+	# To be implemented
+	# crawl_article(soup.find("article"), page_info)
+	crawl_related_guides(soup.find(class_ = "related-guides-content row"), page_info)
 
-	# convert to json object with some indentation
-	json_object = json.dumps(dict, sort_keys=True, indent=4)
-	print json_object
-		
-def crawl_sidebar(soup, dict):
+	return page_info
+
+def crawl_sidebar(soup, page_info):
 	part = "Left Sidebar"
-	dict[part] = {}
-	dict[part]['Title'] = soup.find("div", class_ = "title").a.get_text()
-	dict[part]['Subtitle'] = soup.find("span", class_ = "tagline").a.get_text()
+	page_info[part] = {}
+	page_info[part]['Title'] = soup.find("div", class_ = "title").a.get_text()
+	page_info[part]['Subtitle'] = soup.find("span", class_ = "tagline").a.get_text()
 	
-	dict[part]['Topics'] = []
+	page_info[part]['Topics'] = []
 	for topic in soup.findAll("a", class_ = "sidebar-parent"):
-		dict[part]['Topics'].append(topic.get_text())
+		page_info[part]['Topics'].append(topic.get_text())
 
-def crawl_article(soup, dict):
-	part = "Main Content"
-	dict[part] = {}
-	dict[part]["Article Title"] = soup.find("h1", class_ = "headline").span.get_text()
-	dict[part]["Arthor"] = soup.find("div", class_ = "author").a.get_text().strip()
-
-	dict[part]["Image"] = soup.find(class_ = "build-image").a.img.get('src');
-
-	# get the word content
-	word_content = soup.find(class_ = "markdown-html");
-	dict[part]["Word Content"] = {};
-	# get the three paragraphs with the corresponding names
-	p_names = [];
-	for p_name in word_content.findAll("h2"):
-		p_names.append(p_name.get_text().strip())
-
-	paragraphs = []
-	for paragraph in word_content.findAll("p"):
-		paragraphs.append(paragraph.get_text().strip())
-
-	for n in p_names:
-		for p in paragraphs:
-			dict[part]["Word Content"][n] = p;
-
-
-def crawl_related_guides(soup, dict):
+def crawl_related_guides(soup, page_info):
 	part = "Related Guides"
-	dict[part] = []
+	page_info[part] = []
+	if soup is None:
+		return
 	for guide_block in soup.findAll(class_ = "guide-block"):
-		block = {}
-		block["Title"] = guide_block.find(class_ = "title").a.get_text().strip()
-		block["Subtitle"] = guide_block.find(class_ = "tagline").a.get_text().strip()
-		block["Author"] = guide_block.find(class_ = "author").a.get_text().strip()
-		# block["Gif"] = guide_block.find("video").get('poster')
-		block["Description"] = guide_block.find(class_ = "description").get_text().strip()
-		dict[part].append(block)
+		page_info[part].append(guide_block.find(class_ = "title").a.get_text().strip())
 
+# To be finished
+# def crawl_article(soup, page_info):
+# 	part = "Main Content"
+# 	page_info[part] = {}
+# 	page_info[part]["Article Title"] = soup.find("h1", class_ = "headline").span.get_text()
+# 	page_info[part]["Arthor"] = soup.find("div", class_ = "author").a.get_text().strip()
 
+# 	page_info[part]["Image"] = soup.find(class_ = "build-image").a.img.get('src');
 
-spider()
+# 	# get the word content
+# 	word_content = soup.find(class_ = "markdown-html");
+# 	page_info[part]["Word Content"] = {};
+# 	# get the three paragraphs with the corresponding names
+# 	p_names = [];
+# 	for p_name in word_content.findAll("h2"):
+# 		p_names.append(p_name.get_text().strip())
+
+# 	paragraphs = []
+# 	for paragraph in word_content.findAll("p"):
+# 		paragraphs.append(paragraph.get_text().strip())
+
+# 	for n in p_names:
+# 		for p in paragraphs:
+# 			page_info[part]["Word Content"][n] = p;
+
+crawl()
 
